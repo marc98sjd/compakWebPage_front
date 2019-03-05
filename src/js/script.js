b5d32502fe90a25128eac920abfb84be1468ed49
MarcGuerra <Marc.Guerra@dxcfds.com>
2019-03-05 20:20:08 +0100

    Afegit carousel de corredors utiltizant framework owl

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,40 @@
 $(document).ready(function(){
-    $('.riderCarousel').slick({
-        infinite: true,
-        slidesToShow: 2,
-        slidesToScroll: 1
+    $(".owl-carousel").owlCarousel({
+
+        responsive:{
+            0:{
+                items:1,
+                margin: 10,
+                animateOut: 'fadeOut',
+                responsiveClass:true,
+                nav: false
+            },
+            600:{
+                items:3,
+                margin: 10,
+                animateOut: 'fadeOut',
+                responsiveClass:true,
+                nav:false
+            },
+            1000:{
+                items:5,
+                margin: 25,
+                animateOut: 'fadeOut',
+                responsiveClass:true,
+                nav: false
+            }
+        }
     });
 });
+
+function hover(element) {
+    var src = element.getAttribute('src');
+    var res = src.replace("png", "jpg");
+    element.setAttribute('src', res);
+}
+
+function unhover(element) {
+    var src = element.getAttribute('src');
+    var res = src.replace("jpg", "png")
+    element.setAttribute('src', res);
+}
