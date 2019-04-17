9e921531dedaa7fcded496fa1a875c36b70a8ead
MarcGuerra <Marc.Guerra@dxcfds.com>
2019-04-17 18:41:20 +0200

    separar header i footer per poder injectar-lo

--- src/js/script.js
+++ src/js/script.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
             },
             1000:{
                 items:5,
-                margin: 25,
+                margin: 50,
                 animateOut: 'fadeOut',
                 responsiveClass:true,
                 nav: false
@@ -27,14 +27,38 @@ $(document).ready(function(){
     });
 });
 
-function hover(element) {
-    var src = element.getAttribute('src');
-    var res = src.replace("png", "jpg");
-    element.setAttribute('src', res);
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /*loop through a collection of all HTML elements:*/
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("include-html");
+        if (file) {
+            /*make an HTTP request using the attribute value as the file name:*/
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+                    /*remove the attribute, and call this function once more:*/
+                    elmnt.removeAttribute("include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /*exit the function:*/
+            return;
+        }
+    }
 }
 
-function unhover(element) {
-    var src = element.getAttribute('src');
-    var res = src.replace("jpg", "png")
-    element.setAttribute('src', res);
+let altura = $(".noticias").height();
+function setTwitterHeight(altura) {
+    $(".twitter-timeline").attr('data-height', altura);
 }
+
+includeHTML();
+setTwitterHeight(altura);
